c578664d4ba9f84eb9ce44cc636d2392246797f0
fonsecaderick-spec <fonseca.derick@escola.pr.gov.br>
2026-06-12 09:17:41 -0300

    Update animação.js

--- animação.js
+++ animação.js
@@ -36,70 +36,63 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     const formulario = document.getElementById("formCadastro");
-    if (formulario) {
-        formulario.addEventListener("submit", function(event) {
-            event.preventDefault(); // Impede a página de recarregar
 
-            let nomeFazenda = document.getElementById("cadastro_fazenda").value;
-            let local = document.getElementById("cadastro_localizacao").value;
-            let produtos = document.getElementById("cadastro_produtos").value;
-            let contato = document.getElementById("cadastro_contato").value;
-            let email = document.getElementById("cadastro_email").value;
-            let campoFoto = document.getElementById("foto-produto");
-            let arquivoFoto = campoFoto.files[0];
+if (formulario) {
+    formulario.addEventListener("submit", function(event) {
+        event.preventDefault();
 
-            if (arquivoFoto) {
-                // Salvando os dados de texto no navegador
-                localStorage.setItem("cadastro_fazenda", nomeFazenda);
-                localStorage.setItem("cadastro_local", local);
-                localStorage.setItem("cadastro_produtos", produtos);
-                localStorage.setItem("cadastro_contato", contato);
-                localStorage.setItem("cadastro_email", email);
-                localStorage.setItem("foto-produto", foto);
-                // Convertendo a imagem em texto (Base64) para conseguir passar para a outra página
-                let leitor = new FileReader();
-                leitor.onload = function(e) {
-                    localStorage.setItem("cadastro_foto", e.target.result);
-                    // Só muda de página após terminar de salvar a foto
-                    window.location.href = "conclusao.html";
-                }
-                leitor.readAsDataURL(arquivoFoto);
-            }
-        });
-    }
+        let nomeFazenda = document.getElementById("nome-fazenda").value;
+        let local = document.getElementById("localizacao").value;
+        let produtos = document.getElementById("produtos").value;
+        let contato = document.getElementById("contato").value;
+        let email = document.getElementById("email").value;
+
+        let arquivoFoto =
+        document.getElementById("foto-produto").files[0];
+
+        localStorage.setItem("cadastro_fazenda", nomeFazenda);
+        localStorage.setItem("cadastro_local", local);
+        localStorage.setItem("cadastro_produtos", produtos);
+        localStorage.setItem("cadastro_contato", contato);
+        localStorage.setItem("cadastro_email", email);
+
+        if (arquivoFoto) {
+            let leitor = new FileReader();
+
+            leitor.onload = function(e) {
+                localStorage.setItem(
+                    "cadastro_foto",
+                    e.target.result
+                );
+
+                window.location.href = "conclusao.html";
+            };
+
+            leitor.readAsDataURL(arquivoFoto);
+        } else {
+            window.location.href = "conclusao.html";
+        }
+    });
+}
 
     // --- LÓGICA EXCLUSIVA DA TELA DE CONCLUSÃO ---
-     document.getElementById("formCadastro").addEventListener("submit", function(event){
-        let leitor = new FileReader();
-        leitor.onload = function(e){
-        localStorage.setItem("cadastro_foto", e.target.result);
-        window.location.href = "conclusao.html";
-        };
-
-leitor.readAsDataURL(arquivoFoto);
-
-    event.preventDefault();
-
-    window.location.href = "conclusao.html";
-
-});
     let resumoFazenda = document.getElementById("resumo-fazenda");
     // Se esse elemento existir, significa que o usuário acabou de entrar na tela de conclusão
     if (resumoFazenda) {
         document.getElementById("resumo-fazenda").textContent =
-localStorage.getItem("resumo_fazenda");
+localStorage.getItem("cadastro_fazenda");
 
-        document.getElementById("resumo-local").textContent =
-localStorage.getItem("resumo_local");
+document.getElementById("resumo-local").textContent =
+localStorage.getItem("cadastro_local");
 
-        document.getElementById("resumo-produto").textContent =
-localStorage.getItem("resumo_produtos");
+document.getElementById("resumo-produto").textContent =
+localStorage.getItem("cadastro_produtos");
 
-        document.getElementById("resumo-contato").textContent =
-localStorage.getItem("resumo_contato");
+document.getElementById("resumo-contato").textContent =
+localStorage.getItem("cadastro_contato");
 
-        document.getElementById("resumo-email").textContent =
-localStorage.getItem("resumo_email");
+document.getElementById("resumo-email").textContent =
+localStorage.getItem("cadastro_email");
         
     let fotoSalva = localStorage.getItem("cadastro_foto");
     let tagImg = document.getElementById("resumo-foto");
